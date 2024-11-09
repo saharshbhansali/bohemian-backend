fa2f2f2a8d603102e33d204b443525f80c504c8b
saharshbhansali <saharsh.bhansali15@gmail.com>
2024-11-10 03:12:36 +0530

    Fix: change imports in models
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- application/models.py
+++ application/models.py
@@ -10,8 +10,7 @@ from sqlalchemy import (
     ForeignKey,
     create_engine,
 )
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import relationship, sessionmaker, declarative_base
 from datetime import datetime
 
 
